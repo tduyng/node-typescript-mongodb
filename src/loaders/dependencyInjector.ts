5bc7887415964a2b9c17f0ad8765cfadd25f6faa
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-12-18 11:27:17 +0100

    Remove completely jobs

--- src/loaders/dependencyInjector.ts
+++ src/loaders/dependencyInjector.ts
@@ -3,28 +3,20 @@ import { loggerDev } from 'src/utils/logger';
 import { config } from 'src/config';
 import sgMail from '@sendgrid/mail';
 import { IModelDI } from 'src/types/dependencyInjectors';
-import { agendaFactory } from './agenda';
 
 const dependencyInjector = async ({
-  mongoConnection,
   models,
 }: {
-  mongoConnection;
   models: IModelDI[];
-}) => {
+}): Promise<void> => {
   try {
     models.forEach(m => {
       Container.set(m.name, m.model);
     });
-    const agendaInstance = agendaFactory(mongoConnection);
 
     sgMail.setApiKey(config.emails.apiKey);
-    Container.set('agendaInstance', agendaInstance);
     Container.set('logger', loggerDev);
     Container.set('emailClient', sgMail);
-
-    loggerDev.info('Agenda injected into container');
-    return agendaInstance;
   } catch (error) {
     loggerDev.error(`Error on dependency injector loader: ${error}`);
     throw error;
